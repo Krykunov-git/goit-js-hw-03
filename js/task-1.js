54ce51f871a60b9e630a9aa63ab7cfb5a0259194
Ivan <hyundai27nn@gmail.com>
2024-10-07 22:25:24 +0200

    +task 3
    +3

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,14 @@
 
-// function slugify(title) {
+function slugify(title) {
 
-//     const lol = title.toLowerCase().split(' ').join('-');
-//     console.log(lol);
+    const lol = title.toLowerCase().split(' ').join('-');
+    console.log(lol);
     
     
-//     }
+    }
            
-//     slugify("Arrays for beginners");
-//     slugify("English for developer");
-//     slugify("Ten secrets of JavaScript");
-//     slugify("How to become a JUNIOR developer in TWO WEEKS");
+    slugify("Arrays for beginners");
+    slugify("English for developer");
+    slugify("Ten secrets of JavaScript");
+    slugify("How to become a JUNIOR developer in TWO WEEKS");
     
